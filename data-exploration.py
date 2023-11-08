0caf569f43c5e1c7b22e6d9ec1b9edae77c4493f
iceror <ilsecervor@gmail.com>
2023-11-08 16:17:46 -0600

    comment to dos

--- data-exploration.py
+++ data-exploration.py
@@ -3,10 +3,21 @@ import json
 
 df = pd.read_csv('./spotify_songs.csv')
 
-mask = df['track_artist'] == 'Maroon 5'
-mask_genre = df['playlist_genre'] == 'rock'
+mask_genre = df['playlist_genre'] == 'pop'
 
-filtered_df = df[mask]
 filtered_genre = df[mask_genre]
-print(filtered_genre)
-#print(df['playlist_genre'])
+
+unique_genre = df['playlist_genre'].unique()
+
+for genre in unique_genre:
+  print(genre)
+
+# get most popular song
+# get most danceable songs
+# get most energy songs
+# get songs with higher and lower tempos
+# get shorter and longer songs
+# get num of songs by x artist
+# get num of songs from x year
+# percentage of playlists genre from all playlists genres
+# most popular key (values from 0-11 C-B )
